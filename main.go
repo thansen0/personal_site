ba60dbbd422941f8ead60a1319d4c0f2733c955d
thomas <thomash1996@live.com>
2021-11-25 21:31:38 +0000

    fixed esoteric https bug

--- main.go
+++ main.go
@@ -9,8 +9,9 @@ func main() {
     fs := http.FileServer(http.Dir("./static"))
     http.Handle("/", fs)
 
-    log.Println("Listening on :80...")
-    err := http.ListenAndServe(":80", nil)
+    log.Println("Listening on :443 and :80...")
+    go http.ListenAndServe(":80", nil)
+    err := http.ListenAndServeTLS(":443", "/etc/letsencrypt/live/thomashansen.xyz/fullchain.pem", "/etc/letsencrypt/live/thomashansen.xyz/privkey.pem", nil)
     if err != nil {
         log.Fatal(err)
     }
